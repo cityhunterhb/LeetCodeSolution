22e0d17a52d440c845153ee214d521247c6e5c4e
jack_zheng <cityhunterhb.top@gmail.com>
2019-05-13 09:05:15 +0800

    Update 344_ReverseString.c

--- 344_ReverseString.c
+++ 344_ReverseString.c
@@ -39,8 +39,8 @@ void reverseString(char* s, int sSize){
     for(int i=0; i<(sSize/2); i++)
     {
         temp = s[i];
-        s[i] = s[sSize -1 - i];
-        s[sSize -1 - i] = temp;
+        s[i] = s[sSize - 1 - i];
+        s[sSize - 1 - i] = temp;
     }
 }
 
