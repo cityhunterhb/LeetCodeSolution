0bad3a183f76e72523eae4f9e3404faaf051fb0a
cityhunterhb <cityhunterhb.top@gmail.com>
2019-05-24 07:49:13 +0800

    add 21_MergeTwoSortedList.cpp

---
+++ 21_MergeTwoSortedList.cpp
@@ -0,0 +1,39 @@
+/***************************************************
+*      FileName: 21_MergeTwoSortedList.cpp
+*      Author&website:  cityhunterhb.top
+*      Mail:  cityhunterhb.top@gmail.com
+*      Description: ---
+*      CreatedTime: 2019-05-24 07:48:37
+*      LastModified: 2019-05-24 07:48:44
+****************************************************/
+
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        ListNode dummy(INT_MIN);
+        ListNode *tail = &dummy;
+
+        while (l1 && l2) {
+            if (l1->val < l2->val) {
+                tail->next = l1;
+                l1 = l1->next;
+            } else {
+                tail->next = l2;
+                l2 = l2->next;
+            }
+            tail = tail->next;
+        }
+
+        tail->next = l1 ? l1 : l2;
+        return dummy.next;
+    }
+};
